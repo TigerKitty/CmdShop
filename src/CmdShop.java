809fed986ddd0460d3cfc467a04b805849c91e07
TigerKitty <3210664128@qq.com>
2020-12-26 20:37:24 +0800

    1、从Excle中读取用户数据到数组 2、将输入的用户名、密码进行比对（登录实现）

--- src/CmdShop.java
+++ src/CmdShop.java
@@ -1,3 +1,24 @@
-public class CmdShop {
+import java.io.File;
+import java.util.Scanner;
 
+public class CmdShop {
+    public static void main(String[] args) {
+        File file = new File("D:\\IntelliJ IDEA 2019.2.4\\CmdShop\\src\\users.xlsx");
+        ReadExcel readExcel = new ReadExcel();
+        User[] users = readExcel.readExcel(file);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入用户名");
+        String username = sc.next();
+        System.out.println("请输入密码");
+        String password = sc.next();
+
+        for(int i=0; i<users.length; i++) {
+            System.out.println(users[i].getUsername());
+            System.out.println(users[i].getPassword());
+            if (users[i].getUsername().equals(username) && users[i].getPassword().equals(password)) {
+                System.out.println("登录成功");
+            }
+        }
+    }
 }
